ea69dfac354fd74a4585810145d2616c9e5defa8
SALMAN KHAN <salmankhan.sk302123@gmail.com>
2025-08-10 00:47:24 +0500

    Added APIs pdf

--- Phase1/python/pytest/test_auth.py
+++ Phase1/python/pytest/test_auth.py
@@ -1,3 +1,5 @@
 from expt2 import login
 def test_login():
-    assert login("admin","1235") == True
+    assert login("admin","1234") == True
+def test_login_fail():
+    assert login("user","pass") == False
