98afc21a015e270e7368d745ea9948d7446991cf
SALMAN KHAN <salmankhan.sk302123@gmail.com>
2025-08-20 22:10:39 +0500

    added status code pdf

--- Phase1/FastAPI/app/main.py
+++ Phase1/FastAPI/app/main.py
@@ -3,3 +3,18 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.get("/about")
+def about():
+    return {"About": "This is a sample FastAPI application."}
+# Endpoint to get user information
+@app.get("/users/{user_id}")
+def get_user(user_id: str):
+    return {"user_id": user_id, "name": f"WWelcome  {user_id} to our service!"}
+@app.get("/search")
+def search(query: str):
+    return {"query": query, "results": f"Results for {query}"}
+
+
+
+# Endpoint to get items
